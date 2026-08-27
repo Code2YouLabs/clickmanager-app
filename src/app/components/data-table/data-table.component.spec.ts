9e72ef369a380e3e9b858e8c22644cff956dc066
leonardo.barros <leonardombr@hotmail.com>
2026-08-27 07:36:47 -0300

    style(ui): melhora contraste da toolbar da tabela

--- src/app/components/data-table/data-table.component.spec.ts
+++ src/app/components/data-table/data-table.component.spec.ts
@@ -122,7 +122,7 @@ describe('DataTableComponent', () => {
     expect(fixture.nativeElement.textContent).toContain('Filtros');
     expect(fixture.nativeElement.querySelector('.data-table-toolbar__search')).toBeTruthy();
     expect(fixture.nativeElement.querySelector('.data-table-toolbar__filter-toggle')).toBeTruthy();
-    expect(fixture.nativeElement.querySelector('.data-table-toolbar')?.classList).toContain('bg-light-primary');
+    expect(fixture.nativeElement.querySelector('.data-table-toolbar')?.classList).toContain('bg-light');
     expect(fixture.nativeElement.querySelector('.data-table-toolbar')?.classList).toContain('b-1');
     expect(fixture.nativeElement.querySelector('.data-table-toolbar')?.classList).toContain('rounded');
     expect(fixture.nativeElement.querySelector('.data-table__header-row')).toBeTruthy();
